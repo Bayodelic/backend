e8622144e6a077f00f63f8eafe8fd0f22b5272e7
Braulio Gonzalez <braulio27033@gmail.com>
2024-01-25 12:32:37 -0600

    Importando dependencias y configuracion del proyecto

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,17 @@
 'use strict'
 
+var args = process.argv.slice(2);
+
+var num1 = parseFloat(args[0]);
+var num2 = parseFloat(args[1]);
+
+var plantilla = `
+    La suma es: ${num1 + num2}
+    La resta es: ${num1 - num2}
+    La multiplicacion: ${num1 * num2}
+    La division es: ${num1 / num2}
+`
+
+console.log(args);
+console.log(plantilla);
 console.log("Hola mundo desde node js");
